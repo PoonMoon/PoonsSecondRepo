7936a7b850fff607565cd58d64d0d1dabff3f795
PoonMoon <jerrylandring@gmail.com>
2018-04-12 10:52:14 +0100

    Added a script look

---
+++ Assets/YaasScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class YaasScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	print("Tickle my sausage");
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
